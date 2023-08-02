bff56bde09e16d5fce3ecac79aff4590132c1ba4
prathithkumar <prathithkumarkjk@gmail.com>
2023-08-02 19:03:43 +0200

    practice shell

--- 06-SpecialVariables.sh
+++ 06-SpecialVariables.sh
@@ -14,6 +14,9 @@ echo "Current Topic is $3"
 echo $$     # $$ is going to print the PID of the current process
 echo $#     # $# is going to print the number of arguments
 echo $?     # $? is going to print the exit code of the last command.
+echo $*     # $* is going to print the used arguments
+echo $@     # $@ is goint to print the used arguments
+
 
 #echo "Variables Used In the script $*"  # $* is going to print the used variables
 #echo "Variables Used are $@"            # $@ is going to print the used variables  
