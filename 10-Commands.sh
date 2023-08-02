8af521181bc877240ebe5c4652410bb87c63eab1
prathithkumar <prathithkumarkjk@gmail.com>
2023-08-02 19:45:17 +0200

    practice shell

--- 10-Commands.sh
+++ 10-Commands.sh
@@ -6,3 +6,25 @@
 # 2) alias    ( alias are shortcuts, alias net="netstat -tulpn" )
 # 3) Shell Built-in Commands
 # 4) Functions  # Functions are nothing but a set of command that can be written in a sequence and can be called n number of times as per 
+
+# how to declare a function ?
+
+# below is how we declare a function
+# f() {
+#     echo hai
+# }
+
+# below is how we call a function
+# f
+
+sample(){
+    echo This is a test for command function
+    echo if you see this the function command was successfully executing
+    echo Bye 
+}
+
+sample
+
+echo test call
+
+sample
