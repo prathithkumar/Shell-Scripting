8b9f373a68444078eb0c857dbfbc9f5d4d32430d
prathithkumar <prathithkumarkjk@gmail.com>
2023-08-02 09:41:30 +0200

    practice shell

--- 02- escapeSequence.sh
+++ 02- escapeSequence.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 # \n ---> this indicates a new lines
 # Prathith\nKumar ---> This will print in two lines as we inserted \n in middle.
 # we need to use "-e" for the above line as it was a special character.
