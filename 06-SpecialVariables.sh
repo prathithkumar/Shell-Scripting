ea55a5bc20a024bb21d13568ec30ef5e22cd0bb0
prathithkumar <prathithkumarkjk@gmail.com>
2023-08-02 18:38:31 +0200

    practice shell

--- 06-SpecialVariables.sh
+++ 06-SpecialVariables.sh
@@ -9,3 +9,11 @@ echo $0   # $0 prints the script name we are executing
 echo "Name of recently Launched Rocket in India is $1"
 echo "Popular EV Vehicle in Sweden is $2"
 echo "Current Topic is $3"
+
+
+echo $$     # $$ is going to print the PID of the current process
+echo $#     # $# is going to print the number of arguments
+echo $?     # $? is going to print the exit code of the last command.
+
+echo "Variables Used In the script $*"  # $* is going to print the used variables
+echo "Variables Used are $@"            # $@ is going to print the used variables  
