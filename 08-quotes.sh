4e84c3ac8ad6bc3b14d293945e50951613b1d95b
prathithkumar <prathithkumarkjk@gmail.com>
2023-08-02 19:17:48 +0200

    practice shell

--- 08-quotes.sh
+++ 08-quotes.sh
@@ -8,8 +8,13 @@ echo "$@"     # $@ is goint to print the used arguments
 
 #Here we tried the above commands by inserting double quotes
 
+#Below we are trying with only single quotes lets see the difference
+
 echo '$$'     # $$ is going to print the PID of the current process
 echo '$#'     # $# is going to print the number of arguments
 echo '$?'     # $? is going to print the exit code of the last command.
 echo '$*'     # $* is going to print the used arguments
 echo '$@'     # $@ is goint to print the used arguments
+
+# single quotes will supress the power of a special variable
+# Whenever we have any special variable, prefer to avoid single quotes and go with double quotes
