4aca0ea0eeab22d2f8839e761c48d8866fe9a94f
prathithkumar <prathithkumarkjk@gmail.com>
2023-08-02 10:10:24 +0200

    practice shell

---
+++ 03-colours.sh
@@ -0,0 +1,17 @@
+# Regular Colors
+
+#| Value    | Color  |
+#| -------- | ------ |
+#| \e[0;30m | Black  |
+#| \e[0;31m | Red    |
+#| \e[0;32m | Green  |
+#| \e[0;33m | Yellow |
+#| \e[0;34m | Blue   |
+#| \e[0;35m | Purple |
+#| \e[0;36m | Cyan   |
+#| \e[0;37m | White  |
+
+#sytax to print the colour is:--> echo -e "\e[COLOURCODEm YOUR MSG TO BE PRINTED IN COLOUR \e[0m"
+# echo -e "\e[34m PRATHITH KUMAR \e[0m" 
+
+echo -e "\e[34m PRATHITH KUMAR \e[0m"
