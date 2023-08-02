b2ca7ffd1646ac61e942ff4302a10004a04125ea
prathithkumar <prathithkumarkjk@gmail.com>
2023-08-02 09:29:50 +0200

    practice shell

---
+++ 02- escapeSequence.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Shebang Notation
+
+#! --> This represnts Shebang Notation
+#Shebang Notation tells the script with interpretor this script has to be executed.
+# There can be only one interpretor per script and that should always be the first line of the script
+
+echo Welcome to Bash Scripting!
+
+# How to execute these commands --> $ bash scriptName.sh or $ sh scriptName.sh or ./scriptName.sh
+
+echo Line1
+
+echo Line2
