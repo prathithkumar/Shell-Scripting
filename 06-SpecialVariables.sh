8c4bfd07978fe541babb824c907d9cf5b20986ee
prathithkumar <prathithkumarkjk@gmail.com>
2023-08-02 12:36:03 +0200

    practice shell

---
+++ 06-SpecialVariables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#Special Variables gives special properties to your variables
+
+# Here are few of the special variables : $0 to $9, $? , $# , $* , $@
+
+echo $0   # $0 prints the script name we are executing
+
+echo "Name of recently Launched Rocket in India is $1"
+echo "Popular EV Vehicle in Sweden is $2"
+echo "Current Topic is $3"
