4676bad78b72c3896d0402e3cebada49bffa0eb1
prathithkumar <prathithkumarkjk@gmail.com>
2023-08-02 10:24:28 +0200

    practice shell

--- 03-colours.sh
+++ 03-colours.sh
@@ -11,8 +11,28 @@
 #| \e[0;36m | Cyan   |
 #| \e[0;37m | White  |
 
+
 #sytax to print the colour is:--> echo -e "\e[COLOURCODEm YOUR MSG TO BE PRINTED IN COLOUR \e[0m"
 # echo -e "\e[34m PRATHITH KUMAR \e[0m" 
 
 echo -e "\e[34m PRATHITH KUMAR \e[0m"
 echo -e "\e[31m SITA.dev \e[0m"
+
+# Background
+
+#| Value  | Color  |
+#| ------ | ------ |
+#| \e[40m | Black  |
+#| \e[41m | Red    |
+#| \e[42m | Green  |
+#| \e[43m | Yellow |
+#| \e[44m | Blue   |
+#| \e[45m | Purple |
+#| \e[46m | Cyan   |
+#| \e[47m | White  |
+
+# syntax for backround colours
+
+#echo -e "\e[backGroundCode;foreGroundCodem MESSAGE \e[0m"
+
+echo -e "\e[44;30m TESTING FOR BOTH COLOURS \e[0m"
