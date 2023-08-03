5197aac347923fe1c8f94b3a5ed4acec6ec60690
prathithkumar <prathithkumarkjk@gmail.com>
2023-08-03 17:24:12 +0200

    practice shell

--- 15-ifelse-condition.sh
+++ 15-ifelse-condition.sh
@@ -5,7 +5,7 @@ echo "Demo on if Usage"
 ACTION=$1
 
 if [ "$ACTION" == "start"]; then
-     echo  -e "\e[33m Starting Payment Service \e[0m"
-     exit
+     echo  "Starting Payment"
+     exit 0
 
 fi 
