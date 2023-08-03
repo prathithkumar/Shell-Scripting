d0ea206168b520d32ce25f7658efe6cbf9db5922
prathithkumar <prathithkumarkjk@gmail.com>
2023-08-03 16:45:22 +0200

    practice shell

--- 12-Conditions.sh
+++ 12-Conditions.sh
@@ -16,15 +16,19 @@ ACTION=$1
 case $ACTION in
     start) 
          echo -e "\e[33m Starting Payment Service \e[0m"
+         exit 0
          ;;
     stop)
          echo -e "\e[31m Stopping Payment Service \e[0m"
+         exit 1
          ;;
      restart)
-        echo -e "\e[36m Restarting Payment Service \e[0m"       
+        echo -e "\e[36m Restarting Payment Service \e[0m"
+        exit 2       
         ;;
     *)
         echo -e "\e[35m Valid options are start or stop or restart \e[0m"
         echo -e "\e[36m Example Usage \e[0m :\n \t Bash scriptName Stop"
+        exit 3
         ;;
 esac
