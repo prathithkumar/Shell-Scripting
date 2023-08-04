9e05f0fe34d1a0f8e0661b515dcb934da5a2aabc
prathithkumar <prathithkumarkjk@gmail.com>
2023-08-04 13:24:24 +0200

    practice shell

--- roboshop/Components/frontend.sh
+++ roboshop/Components/frontend.sh
@@ -8,9 +8,9 @@ if [ $USER_ID -ne 0 ] ; then
     echo -e "\e[32m Script is expected to executed by the root user or with a sudo privilage \e[0m \n \t Example: sudo bash wrapper.sh frontend"
     exit 1
 fi 
-   
+
 echo "Configuring frontend"
-yum install nginx -y
+yum install nginx -y &>> /tmp /frontend.log
 
 # yum install nginx -y
 # systemctl enable nginx
