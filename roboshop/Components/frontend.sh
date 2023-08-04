2dd05dafdc4ab56325392e2e84b4ab275c61ca8c
prathithkumar <prathithkumarkjk@gmail.com>
2023-08-04 13:06:32 +0200

    practice shell

--- roboshop/Components/frontend.sh
+++ roboshop/Components/frontend.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 
-echo "I am Front End"
+echo "Configuring frontend"
+yum install nginx -y
+
+# yum install nginx -y
+# systemctl enable nginx
+# systemctl start nginx
+# curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
+
+# cd /usr/share/nginx/html
+# rm -rf *
+# unzip /tmp/frontend.zip
+# mv frontend-main/* .
+# mv static/* .
+# rm -rf frontend-main README.md
+# mv localhost.conf /etc/nginx/default.d/roboshop.conf
