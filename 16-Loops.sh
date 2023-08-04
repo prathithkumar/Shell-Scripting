ba63b1280f288817b5236f7233987c97cafbf68f
prathithkumar <prathithkumarkjk@gmail.com>
2023-08-04 12:10:18 +0200

    practice shell

---
+++ 16-Loops.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+# Loops are used top execute something for certain number of times
+
+
+# 1) For Loops       (When you know something to be executed n number of times we use FOR Loop)
+# 2) While Loops     (When you dont know something to be executed n number of times, we use while Loop)
+
+# For Loop Syntax
+
+for val in 10 20 30 40 50; do
+    echo "Value for the Loop is $val"
+done    
