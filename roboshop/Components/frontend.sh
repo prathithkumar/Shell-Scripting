cfc31de8ec4005e4eacf63d2206e43dc4169cda8
prathithkumar <prathithkumarkjk@gmail.com>
2023-08-07 17:54:39 +0200

    practice shell

--- roboshop/Components/frontend.sh
+++ roboshop/Components/frontend.sh
@@ -37,7 +37,7 @@ echo -n "Downloading the ${COMPONENT} component:"
 curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/${COMPONENT}/archive/main.zip"
 stat $?
 
-echo -n "Clean up of ${COMPONENT} :"
+echo -n "Clean up of ${COMPONENT} : "
 cd /usr/share/nginx/html
 rm -rf *   &>> ${LOGFILE}
 stat $?
