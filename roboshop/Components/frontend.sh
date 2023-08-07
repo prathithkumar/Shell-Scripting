c6ae967534503fb8fcb6681ad849e041bcd8cbac
prathithkumar <prathithkumarkjk@gmail.com>
2023-08-07 16:35:12 +0200

    practice shell

--- roboshop/Components/frontend.sh
+++ roboshop/Components/frontend.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-
+set -e
 # Validate the user who is running the script is a root user or not.
 
 USER_ID=$(id -u)
+COMPONENT=$1
+LOGFILE=*/tmp/${COMPONENT}.log*
 
 if [ $USER_ID -ne 0 ] ; then
     echo -e "\e[32m Script is expected to executed by the root user or with a sudo privilage \e[0m \n \t Example: \n\t\t sudo bash wrapper.sh frontend"
@@ -19,43 +21,41 @@ stat() {
     fi 
 }
 
-echo "Configuring frontend"
+echo "Configuring ${COMPONENT}"
 
-echo -n "Installing Frontend"
-yum install nginx -y    &>> /tmp/frontend.log
+echo -n "Installing Nginx"
+yum install nginx -y   &>> ${LOGFILE}
 stat $?
 
 
 echo -n "Starting Nginx"
-systemctl enable nginx     &>> /tmp/frontend.log
-systemctl start nginx      &>> /tmp/frontend.log
+systemctl enable nginx     &>> ${LOGFILE}
+systemctl start nginx      &>> ${LOGFILE}
 stat $?
 
 
-echo -n "Downloading the frontend component:"
-curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
+echo -n "Downloading the ${COMPONENT} component:"
+curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/${COMPONENT}/archive/main.zip"
 stat $?
 
-echo -n "Clean up of frontend :"
+echo -n "Clean up of ${COMPONENT} :"
 cd /usr/share/nginx/html
-rm -rf *   &>> /tmp/frontend.log
+rm -rf *   &>> ${LOGFILE}
 stat $?
 
-echo -n "Extracting Frontend :" 
-unzip  /tmp/frontend.zip  &>> /tmp/frontend.log
-stat $?
-
-echo -n "Sorting front end files :"
-mv frontend-main/* .
+echo -n "Extracting ${COMPONENT} :" 
+unzip  /tmp/frontend.zip  &>> ${LOGFILE}
+mv ${COMPONENT}-main/*
 mv static/* .
-rm -rf frontend-main README.md
+rm -rf ${COMPONENT}-main README.md
 mv localhost.conf /etc/nginx/default.d/roboshop.conf
 stat $?
 
 
-echo -n "Restarting Frontend"
-systemctl daemon-reload    &>> /tmp/frontend.log
-systemctl restart nginx    &>> /tmp/frontend.log
+echo -n "Restarting ${COMPONENT}"
+systemctl daemon-reload    &>> ${LOGFILE}
+systemctl restart nginx    &>> ${LOGFILE}
+stat $?
 
 # I want to ensure, that SCRIPT SHOULD Fail the user who run the script is not a root user.
 # rather executing the commands and failing.
