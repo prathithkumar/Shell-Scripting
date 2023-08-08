4c8d57e2a86d59db075c5f78baac40a7a3996d0e
prathithkumar <prathithkumarkjk@gmail.com>
2023-08-08 11:23:46 +0200

    practice shell

--- roboshop/Components/catalogue.sh
+++ roboshop/Components/catalogue.sh
@@ -32,7 +32,7 @@ yum install nodejs -y  &>> ${LOGFILE}
 stat $?
 
 id ${APPUSER}          &>> ${LOGFILE}
-if [$? -ne 0] ; then
+if [ $? -ne 0 ] ; then
     echo -n "Creating Application User Account :"
     useradd roboshop
     stat $?
