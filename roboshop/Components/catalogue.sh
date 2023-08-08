668733a94b8908d8464310d5b1359b3490ea6843
prathithkumar <prathithkumarkjk@gmail.com>
2023-08-08 11:33:55 +0200

    practice shell

--- roboshop/Components/catalogue.sh
+++ roboshop/Components/catalogue.sh
@@ -38,6 +38,11 @@ if [ $? -ne 0 ] ; then
     stat $?
 fi     
 
+echo -n "Downloading the ${COMPONENT} : "
+curl -s -L -o /tmp/${COMPONENT}.zip "https://github.com/stans-robot-project/${COMPONENT}/archive/main.zip"
+stat $?
 
-
-
+echo -n "Copying the ${COMPONENT} to ${APPUSER} home directory"
+cd /home/${APPUSER}/
+rm -rf ${COMPONENT}    &>> ${LOGFILE}
+unzip -o /tmp/${COMPONENT}.zip  &>> ${LOGFILE}
