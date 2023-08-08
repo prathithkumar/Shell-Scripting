19f2892e64848820a51ea807af5a4556f6672fd9
prathithkumar <prathithkumarkjk@gmail.com>
2023-08-08 12:04:38 +0200

    practice shell

--- roboshop/Components/catalogue.sh
+++ roboshop/Components/catalogue.sh
@@ -57,3 +57,13 @@ echo -n "Generating the ${COMPONENT} artifacts : "
 cd /home/${APPUSER}/${COMPONENT}/
 npm install       &>> ${LOGFILE}
 stat $?
+
+echo -n "Configuring the ${COMPONENT} system file :"
+sed -ie 's/MONGO_DNSNAME/mongodb.roboshop.internal/' /home/${APPUSER}/${COMPONENT}/systemd.service
+mv /home/${APPUSER}/${COMPONENT}/systemd.service /etc/systemd/system/${COMPONENT}.service
+stat $?
+
+echo -n "Starting the ${COMPONENT} service :"
+systemctl daemon-reload &>> ${LOGFILE}
+systemctl enable ${COMPONENT} &>> ${LOGFILE}
+systemctl restart ${COMPONENT} &>> ${LOGFILE}
