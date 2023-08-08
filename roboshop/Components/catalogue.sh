b054e13515e620dbd2ab2bda97a27e36b059a556
prathithkumar <prathithkumarkjk@gmail.com>
2023-08-08 11:42:11 +0200

    practice shell

--- roboshop/Components/catalogue.sh
+++ roboshop/Components/catalogue.sh
@@ -47,3 +47,8 @@ cd /home/${APPUSER}/
 rm -rf ${COMPONENT}             &>> ${LOGFILE}
 unzip -o /tmp/${COMPONENT}.zip  &>> ${LOGFILE}
 stat $?
+
+echo -n "Changing the ownership :"
+mv ${COMPONENT}-main ${COMPONENT}
+chown -R ${APPUSER}:${APPUSER} /home/${APPUSER}/${COMPONENT}/
+stat $?
