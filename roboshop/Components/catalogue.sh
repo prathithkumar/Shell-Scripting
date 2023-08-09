e8312c30594292ef67989dd150accc1645d27eb2
prathithkumar <prathithkumarkjk@gmail.com>
2023-08-09 17:50:46 +0200

    practice shell

--- roboshop/Components/catalogue.sh
+++ roboshop/Components/catalogue.sh
@@ -1,72 +1,32 @@
 #!/bin/bash
 
-# Validate the user who is running the script is a root user or not.
-
-USER_ID=$(id -u)
 COMPONENT=catalogue
-LOGFILE="/tmp/${COMPONENT}.log"
-APPUSER="roboshop"
 
-if [ $USER_ID -ne 0 ] ; then
-    echo -e "\e[32m Script is expected to executed by the root user or with a sudo privilage \e[0m \n \t Example: \n\t\t sudo bash wrapper.sh frontend"
-    exit 1
-fi 
+#This is how we import the functions that are declared ina different file using source
+source Components/common.sh
 
-stat() {
-    if [ $1 -eq 0 ]; then 
-      echo -e "\e[32m success \e[0m"
-    else
-      echo  -e "\e[31m failure \e[0m"
-      exit 2
-    fi 
-}
+NODEJS    # calling nodejs function.
 
-echo "Configuring ${COMPONENT}"
 
-echo -n "Configuring ${COMPONENT} repo"
-curl --silent --location https://rpm.nodesource.com/setup_16.x |  bash - &>> ${LOGFILE}
-stat $?
 
-echo -n "Installing NodeJS :"
-yum install nodejs -y  &>> ${LOGFILE}
-stat $?
 
-id ${APPUSER}          &>> ${LOGFILE}
-if [ $? -ne 0 ] ; then
-    echo -n "Creating Application User Account :"
-    useradd roboshop
-    stat $?
-fi     
+source com
 
-echo -n "Downloading the ${COMPONENT} : "
-curl -s -L -o /tmp/${COMPONENT}.zip "https://github.com/stans-robot-project/${COMPONENT}/archive/main.zip"
-stat $?
 
-echo -n "Copying the ${COMPONENT} to ${APPUSER} home directory"
-cd /home/${APPUSER}/
-rm -rf ${COMPONENT}             &>> ${LOGFILE}
-unzip -o /tmp/${COMPONENT}.zip  &>> ${LOGFILE}
-stat $?
 
-echo -n "Changing the ownership :"
-mv ${COMPONENT}-main ${COMPONENT}
-chown -R ${APPUSER}:${APPUSER} /home/${APPUSER}/${COMPONENT}/
-stat $?
 
-echo -n "Generating the ${COMPONENT} artifacts : "
-cd /home/${APPUSER}/${COMPONENT}/
-npm install       &>> ${LOGFILE}
-stat $?
 
-echo -n "Configuring the ${COMPONENT} system file :"
-sed -ie 's/MONGO_DNSNAME/mongodb.roboshop.internal/' /home/${APPUSER}/${COMPONENT}/systemd.service
-mv /home/${APPUSER}/${COMPONENT}/systemd.service /etc/systemd/system/${COMPONENT}.service
-stat $?
 
-echo -n "Starting the ${COMPONENT} service :"
-systemctl daemon-reload &>> ${LOGFILE}
-systemctl enable ${COMPONENT} &>> ${LOGFILE}
-systemctl restart ${COMPONENT} &>> ${LOGFILE}
-stat $?
+
+  
+
+
+
+
+
+
+
+
+
 
 echo -e "\e[35m ${COMPONENT} Installation is completed \e[0m \n"
---
+++ roboshop/Components/common.sh
@@ -0,0 +1,92 @@
+# All the common fucntions will be declared here. Rest of the components will be sourcing the functions from its files.
+
+COMPONENT=catalogue
+LOGFILE="/tmp/${COMPONENT}.log"
+APPUSER="roboshop"
+
+USER_ID=$(id -u)
+
+if [ $USER_ID -ne 0 ] ; then
+    echo -e "\e[32m Script is expected to executed by the root user or with a sudo privilage \e[0m \n \t Example: \n\t\t sudo bash wrapper.sh frontend"
+    exit 1
+fi
+
+stat() {
+    if [ $1 -eq 0 ]; then 
+      echo -e "\e[32m success \e[0m"
+    else
+      echo  -e "\e[31m failure \e[0m"
+      exit 2
+    fi 
+}
+# Function to create a user account
+CREATE_USER(){
+    id ${APPUSER}          &>> ${LOGFILE}
+    if [ $? -ne 0 ] ; then
+    echo -n "Creating Application User Account :"
+    useradd roboshop
+    stat $?
+    fi   
+}
+
+
+DOWNLOAD_AND_EXTRACT() {
+
+        echo -n "Downloading the ${COMPONENT} : "
+        curl -s -L -o /tmp/${COMPONENT}.zip "https://github.com/stans-robot-project/${COMPONENT}/archive/main.zip"
+        stat $?
+
+        cd /home/${APPUSER}/
+        rm -rf ${COMPONENT}             &>> ${LOGFILE}
+        unzip -o /tmp/${COMPONENT}.zip  &>> ${LOGFILE}
+        stat $?
+
+        echo -n "Changing the ownership :"
+        mv ${COMPONENT}-main ${COMPONENT}
+        chown -R ${APPUSER}:${APPUSER} /home/${APPUSER}/${COMPONENT}/
+        stat $?
+
+}
+
+CONFIG_SVC(){
+    
+    echo -n "Configuring the ${COMPONENT} system file :"
+    sed -ie 's/MONGO_DNSNAME/mongodb.roboshop.internal/' /home/${APPUSER}/${COMPONENT}/systemd.service
+    mv /home/${APPUSER}/${COMPONENT}/systemd.service /etc/systemd/system/${COMPONENT}.service
+    stat $?
+
+    echo -n "Starting the ${COMPONENT} service :"
+    systemctl daemon-reload &>> ${LOGFILE}
+    systemctl enable ${COMPONENT} &>> ${LOGFILE}
+    systemctl restart ${COMPONENT} &>> ${LOGFILE}
+    stat $?
+
+
+}
+
+
+
+
+
+# Declaring a NodeJS function
+NodeJS() {
+
+        echo "Configuring ${COMPONENT}"
+
+        echo -n "Configuring ${COMPONENT} repo :"
+        curl --silent --location https://rpm.nodesource.com/setup_16.x |  bash - &>> ${LOGFILE}
+        stat $?
+
+        echo -n "Installing NodeJS :"
+        yum install nodejs -y  &>> ${LOGFILE}
+        stat $?
+ 
+       CREATE_USER            # Calls CREATE_USER functiom that creates User account:
+
+       DOWNLOAD_AND_EXTRACT   # Downloading and extracts the components
+
+       echo -n "Generating the ${COMPONENT} artifacts : "
+       cd /home/${APPUSER}/${COMPONENT}/
+       npm install       &>> ${LOGFILE}
+       stat $? 
+} 
