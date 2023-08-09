215c91627ba0eb3e93f34ed7c31ab5d1c7dfc70b
prathithkumar <prathithkumarkjk@gmail.com>
2023-08-09 19:43:45 +0200

    practice shell

--- roboshop/Components/mysql.sh
+++ roboshop/Components/mysql.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 
-echo "mysql"
+COMPONENT=mysql
+
+source Components/common.sh
+
+echo "Configuring ${COMPONENT}"
+
+echo -n "Configuring ${COMPONENT} repo :"
+curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/stans-robot-project/mysql/main/mysql.repo 
+stat $?
+
+echo -n "Installing ${COMPONENT}"
+yum install mysql-community-server -y   &>> ${LOGFILE}
+stat $?
+
+
