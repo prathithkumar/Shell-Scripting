4d281080d3eef9d603f5d37e8d2af213447c5064
prathithkumar <prathithkumarkjk@gmail.com>
2023-08-10 10:34:31 +0200

    practice shell

--- roboshop/Components/frontend.sh
+++ roboshop/Components/frontend.sh
@@ -50,10 +50,10 @@ stat $?
 
 echo -n "Updating the Backend Components in the reverse proxy file: "
 
-for component in catalogue user cart ;do
+for component in catalogue user Cart ;do
     sed -i  -e "/${component}/s/localhost/${component}.roboshop.internal/" /etc/nginx/default.d/roboshop.conf
 done
-  
+
 echo -n "Restarting ${COMPONENT}:"
 systemctl daemon-reload    &>> ${LOGFILE}
 systemctl restart nginx    &>> ${LOGFILE}
