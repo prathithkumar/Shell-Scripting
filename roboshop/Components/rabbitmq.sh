3665e5f68e1697cbeb27f45ec21ee4ec4d10d463
prathithkumar <prathithkumarkjk@gmail.com>
2023-08-22 16:08:31 +0200

    practice shell

---
+++ roboshop/Components/rabbitmq.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Validate the user who is running the script is a root user or not.
+COMPONENT=rabbitmq
+
+
+source Components/common.sh
+
+
+echo "Configuring ${COMPONENT}"
+
+echo -n "Configuring ${COMPONENT} repositories: "
+curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | bash &>> ${LOGFILE}
+curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | bash &>> ${LOGFILE}
+stat $?
+
+echo -n "Installing ${COMPONENT} : "
+yum install rabbitmq-server -y &>> ${LOGFILE}
+stat $?
+
+echo -n "Starting {$COMPONENT}"
+systemctl enable rabbitmq-server &>> ${LOGFILE}
+systemctl start rabbitmq-server  &>> ${LOGFILE}
+stat $?
+
+echo -n "Creating ${COMPONENT} user account :"
+rabbitmqctl add_user roboshop roboshop123
+stat $?
+
+echo -n "Configuring the permissions : "
+rabbitmqctl add_user roboshop roboshop123
+rabbitmqctl set_user_tags roboshop administrator
+rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
+stat $?
+
+
+echo -e "Installation is Completed"
