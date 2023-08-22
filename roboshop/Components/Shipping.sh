5921fa71a68232fa4c39f4fdb2ca27a8c3d34022
prathithkumar <prathithkumarkjk@gmail.com>
2023-08-22 15:38:40 +0200

    practice shell

--- roboshop/Components/Shipping.sh
+++ roboshop/Components/Shipping.sh
@@ -5,7 +5,7 @@ COMPONENT=shipping
 #This is how we import the functions that are declared ina different file using source
 source Components/common.sh
 
-NODEJS    # calling nodejs function.
+JAVA    # calling nodejs function.
 
 
 echo -e "\e[35m ${COMPONENT} Installation is completed \e[0m \n"
