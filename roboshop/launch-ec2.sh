8c40b2081af4ebf006810548615062227c03e7d2
prathithkumar <prathithkumarkjk@gmail.com>
2023-08-23 16:21:18 +0200

    practice shell

--- roboshop/launch-ec2.sh
+++ roboshop/launch-ec2.sh
@@ -8,13 +8,17 @@ if [ -z $1 ] ; then
 fi  
 
 
-AMI_ID="ami-0c1d144c8fdd8d690"
+AMI_ID="$(aws ec2 describe-images --filters "Name=name,Values=DevOps-LabImage-CentOS7"| jq ".Images[].ImageId" | sed -e 's/"//g')"
 INSTANCE_TYPE="t3.micro"
 SG_ID="sg-0e1221dcbe704dc32"
 
-PRIVATEIP=$(aws ec2 run-instances --image-id ${AMI_ID} --instance-type ${INSTANCE_TYPE}  --security-group-ids ${SG_ID} --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" | jq '.Instances[].PrivateIpAddress')
+
+echo  -e "****** Creating ${COMPONENT} Server Is In Progress *******"
+PRIVATEIP=$(aws ec2 run-instances --image-id ${AMI_ID} --instance-type ${INSTANCE_TYPE}  --security-group-ids ${SG_ID} --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" | jq '.Instances[].PrivateIpAddress' |  sed -e 's/"//g')
 
 echo "Private IP Address of the $COMPONENT is $PRIVATEIP"
+
+aws ec2 describe-security-groups --filters Name=group-name,Values=b55-allow-all
 
 
 
