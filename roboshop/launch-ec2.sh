f1e7e10a2a08a928a8b7e09cb78fd720f6de3391
prathithkumar <prathithkumarkjk@gmail.com>
2023-08-23 17:34:30 +0200

    practice shell

--- roboshop/launch-ec2.sh
+++ roboshop/launch-ec2.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 COMPONENT=$1
+INSTANCE_TYPE="t3.micro"
+HOSTEDZONEID="Z05528692BAIDU4FZAFQL"
+
+
 if [ -z $1 ] ; then 
    echo -e "COMPONENT NAME IS NEEDED"
    echo -e "Ex Usage $ bash launch-ec2.sh shipping"
@@ -9,9 +13,9 @@ fi
 
 
 AMI_ID="$(aws ec2 describe-images --filters "Name=name,Values=DevOps-LabImage-CentOS7"| jq ".Images[].ImageId" | sed -e 's/"//g')"
-INSTANCE_TYPE="t3.micro"
+
 SG_ID="$(aws ec2 describe-security-groups --filters Name=group-name,Values=b55-allowall | jq '.SecurityGroups[].GroupId' | sed -e 's/"//g')"
-HOSTEDZONEID="Z05528692BAIDU4FZAFQL"
+
 
 echo  -e "****** Creating ${COMPONENT} Server Is In Progress *******"
 PRIVATEIP=$(aws ec2 run-instances --image-id ${AMI_ID} --instance-type ${INSTANCE_TYPE}  --security-group-ids ${SG_ID} --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" | jq '.Instances[].PrivateIpAddress' |  sed -e 's/"//g')
