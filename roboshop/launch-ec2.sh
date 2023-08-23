8b3e9f9d8c528128be5ccfee05854708f7b104b4
prathithkumar <prathithkumarkjk@gmail.com>
2023-08-23 17:39:46 +0200

    practice shell

--- roboshop/launch-ec2.sh
+++ roboshop/launch-ec2.sh
@@ -11,10 +11,11 @@ if [ -z $1 ] ; then
    exit 1
 fi  
 
-
 AMI_ID="$(aws ec2 describe-images --filters "Name=name,Values=DevOps-LabImage-CentOS7"| jq ".Images[].ImageId" | sed -e 's/"//g')"
-
 SG_ID="$(aws ec2 describe-security-groups --filters Name=group-name,Values=b55-allowall | jq '.SecurityGroups[].GroupId' | sed -e 's/"//g')"
+
+create_ec2(){
+
 
 
 echo  -e "****** Creating ${COMPONENT} Server Is In Progress *******"
@@ -26,7 +27,19 @@ echo -e "Creating DNS Record of ${COMPONENT} :"
 sed -e "s/COMPONENT/${COMPONENT}/" -e "s/IPADDRESS/${PRIVATEIP}/" route53.json  >  /tmp/r53.json
 aws route53 change-resource-record-sets --hosted-zone-id $HOSTEDZONEID --change-batch file:///tmp/r53.json
 echo -e "Private IP Address of the $COMPONENT is created and ready to use on ${COMPONENT}.roboshop.internal"
+}
 
+
+if [ "$1" == "all" ]; then
+
+    for component in mongodb catalogue cart user shipping frontend payment mysql redis rabbitmq; do
+        COMPONENT=$component
+        create_ec2
+    done
+
+else    
+      create_ec2
+fi
 
 
 
