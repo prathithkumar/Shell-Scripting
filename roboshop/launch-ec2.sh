7cadf684a591547786bac39ed809c903138d3a73
prathithkumar <prathithkumarkjk@gmail.com>
2023-08-25 14:32:43 +0200

    practice shell

--- roboshop/launch-ec2.sh
+++ roboshop/launch-ec2.sh
@@ -18,15 +18,16 @@ create_ec2(){
 
 
 
-echo  -e "****** Creating ${COMPONENT} Server Is In Progress *******"
-PRIVATEIP=$(aws ec2 run-instances --image-id ${AMI_ID} --instance-type ${INSTANCE_TYPE}  --security-group-ids ${SG_ID} --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" | jq '.Instances[].PrivateIpAddress' |  sed -e 's/"//g')
+    echo  -e "****** Creating ${COMPONENT} Server Is In Progress *******"
+    PRIVATEIP=$(aws ec2 run-instances --image-id ${AMI_ID} --instance-type ${INSTANCE_TYPE}  --security-group-ids ${SG_ID} --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" | jq '.Instances[].PrivateIpAddress' |  sed -e 's/"//g')
 
-echo -e "Private IP Address of the $COMPONENT is $PRIVATEIP \n\n"
-echo -e "Creating DNS Record of ${COMPONENT} :"
+    echo -e "Private IP Address of the $COMPONENT is $PRIVATEIP \n\n"
+    echo -e "Creating DNS Record of ${COMPONENT} :"
 
-sed -e "s/COMPONENT/${COMPONENT}/" -e "s/IPADDRESS/${PRIVATEIP}/" route53.json  >  /tmp/r53.json
-aws route53 change-resource-record-sets --hosted-zone-id $HOSTEDZONEID --change-batch file:///tmp/r53.json
-echo -e "Private IP Address of the $COMPONENT is created and ready to use on ${COMPONENT}.roboshop.internal"
+    sed -e "s/COMPONENT/${COMPONENT}/" -e "s/IPADDRESS/${PRIVATEIP}/" route53.json  >  /tmp/r53.json
+    
+    aws route53 change-resource-record-sets --hosted-zone-id $HOSTEDZONEID --change-batch file:///tmp/r53.json
+    echo -e "Private IP Address of the $COMPONENT is created and ready to use on ${COMPONENT}.roboshop.internal"
 }
 
 
@@ -38,7 +39,7 @@ if [ "$1" == "all" ]; then
     done
 
 else    
-      create_ec2
+        create_ec2
 fi
 
 
